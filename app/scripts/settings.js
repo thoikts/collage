491c9aa1bd5d5be7309f6500c18b2adbe427ce31
thoikts <thoikts@gmail.com>
2013-10-04 00:37:41 +0700

    Update settings.js

--- app/scripts/settings.js
+++ app/scripts/settings.js
@@ -2,7 +2,7 @@ define(function(){
 
     return {
         //make sure this is set to point to a valid Filepicker application key
-        filepickerApplicationKey : "",
+        filepickerApplicationKey : "AzPinWkFNTxGdweAVKqTTz",
         collageWidth : 480,
         collageHeight : 480
     };
